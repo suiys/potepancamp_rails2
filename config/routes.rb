4985fe3d0fa9df78b4dd0a06ca99db96e4cf0e99
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-08-13 00:26:55 +0900

    新規登録後に遷移するプロフィール画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/profile'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
   get '/', to: 'home#top', as: 'root'
+
+  get 'users/profile', to: 'users#profile', as: 'profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
