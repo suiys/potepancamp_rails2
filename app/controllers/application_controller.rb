7f46ced389366fe0dbb308965000871c81289dab
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-08-13 22:24:48 +0900

    アカウント側の設定ページではimageを編集しないのでpermitted_parametersに追加しない

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_perameter_sanitizer.permit(:account_update, keys: %i(image))
   end
 end
