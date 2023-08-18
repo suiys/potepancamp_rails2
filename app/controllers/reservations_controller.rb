e2c249f4be744035a1e063121b2a855904f2117c
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-08-19 06:14:47 +0900

    予約確認ページのビューを作成

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -26,4 +26,17 @@ class ReservationsController < ApplicationController
     
   end
 
+  def confirm
+    @reservation = Reservation.new(reservation_params)
+    @stay_for = (@reservation.check_out - @reservation.check_in).to_i
+    @total_charge = @reservation.room.room_charge * @stay_for * @reservation.person
+    if @reservation.invalid?
+      flash.now[:notice] = "予約情報に不備があります"
+      render "rooms/show"
+    end
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:check_in, :check_out, :person, :room_id)
+  end
 end
